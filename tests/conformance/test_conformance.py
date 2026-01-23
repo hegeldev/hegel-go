8795b3fe55861b7a74c801ddbae2e3d7df53dbbc
Liam DeVoe <orionldevoe@gmail.com>
2026-01-23 13:37:16 -0500

    update for new conformance tests

--- tests/conformance/test_conformance.py
+++ tests/conformance/test_conformance.py
@@ -2,37 +2,21 @@ from pathlib import Path
 
 import pytest
 
-from hegel.conformance import run_conformance_test
+from hegel.conformance import conformance_tests, run_conformance_test
 
 BUILD_DIR = Path(__file__).parent / "go" / "bin"
 
-
-def test_booleans():
-    run_conformance_test("booleans", BUILD_DIR / "test_booleans")
-
-
-def test_integers():
-    run_conformance_test("integers", BUILD_DIR / "test_integers")
-
-
-def test_floats():
-    run_conformance_test("floats", BUILD_DIR / "test_floats")
+TESTS = conformance_tests({
+    "booleans": BUILD_DIR / "test_booleans",
+    "integers": BUILD_DIR / "test_integers",
+    "floats": BUILD_DIR / "test_floats",
+    "text": None,  # skipped due to hypothesis-jsonschema bug with Unicode surrogate pairs
+    "binary": BUILD_DIR / "test_binary",
+    "lists": BUILD_DIR / "test_lists",
+    "sampled_from": BUILD_DIR / "test_sampled_from",
+})
 
 
-@pytest.mark.skip(
-    reason="Disabled due to hypothesis-jsonschema bug with Unicode surrogate pairs"
-)
-def test_text():
-    run_conformance_test("text", BUILD_DIR / "test_text")
-
-
-def test_lists():
-    run_conformance_test("lists", BUILD_DIR / "test_lists")
-
-
-def test_sampled_from():
-    run_conformance_test("sampled_from", BUILD_DIR / "test_sampled_from")
-
-
-def test_binary():
-    run_conformance_test("binary", BUILD_DIR / "test_binary")
+@pytest.mark.parametrize("test_name,binary_path", TESTS, ids=[t[0] for t in TESTS])
+def test_conformance(test_name, binary_path):
+    run_conformance_test(test_name, binary_path)
