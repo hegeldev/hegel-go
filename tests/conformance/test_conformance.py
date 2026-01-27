fa327ba80ccfee2795ac290359cfad76bd15dc4a
Liam DeVoe <orionldevoe@gmail.com>
2026-01-26 20:21:54 -0500

    restrict conformance range

--- tests/conformance/test_conformance.py
+++ tests/conformance/test_conformance.py
@@ -13,16 +13,23 @@ from hegel.conformance import (
 
 BUILD_DIR = Path(__file__).parent / "go" / "bin"
 
+INT64_MIN = -(2**63)
+INT64_MAX = 2**63 - 1
+
 
 def test_conformance(subtests):
     run_conformance_tests(
         [
             BooleanConformance(BUILD_DIR / "test_booleans"),
-            IntegerConformance(BUILD_DIR / "test_integers"),
+            IntegerConformance(
+                BUILD_DIR / "test_integers", min_value=INT64_MIN, max_value=INT64_MAX
+            ),
             FloatConformance(BUILD_DIR / "test_floats"),
             TextConformance(BUILD_DIR / "test_text"),
             BinaryConformance(BUILD_DIR / "test_binary"),
-            ListConformance(BUILD_DIR / "test_lists"),
+            ListConformance(
+                BUILD_DIR / "test_lists", min_value=INT64_MIN, max_value=INT64_MAX
+            ),
             SampledFromConformance(BUILD_DIR / "test_sampled_from"),
         ],
         subtests,
